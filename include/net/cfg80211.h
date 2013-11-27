3fd09f129d82be23539bf0e571e4adc9ce5797c5
kasperhettinga <kasper.hettinga@gmail.com>
2013-11-27 19:43:25 +0100

    Revert "nl80211: advertise device AP SME"
    This reverts commit 4a3124f3162a77f70c2d1ab1cbbd417c7ceb227a.

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -1623,7 +1623,6 @@ struct cfg80211_ops {
  *	teardown packets should be sent through the @NL80211_CMD_TDLS_MGMT
  *	command. When this flag is not set, @NL80211_CMD_TDLS_OPER should be
  *	used for asking the driver/firmware to perform a TDLS operation.
- * @WIPHY_FLAG_HAVE_AP_SME: device integrates AP SME
  */
 enum wiphy_flags {
 	WIPHY_FLAG_CUSTOM_REGULATORY		= BIT(0),
@@ -1642,7 +1641,6 @@ enum wiphy_flags {
 	WIPHY_FLAG_AP_UAPSD			= BIT(14),
 	WIPHY_FLAG_SUPPORTS_TDLS		= BIT(15),
 	WIPHY_FLAG_TDLS_EXTERNAL_SETUP		= BIT(16),
-	WIPHY_FLAG_HAVE_AP_SME			= BIT(17),
 };
 
 /**
@@ -1852,8 +1850,6 @@ struct wiphy_wowlan_support {
  *	may request, if implemented.
  *
  * @wowlan: WoWLAN support information
- *
- * @ap_sme_capa: AP SME capabilities, flags from &enum nl80211_ap_sme_features.
  */
 struct wiphy {
 	/* assign these fields before you register the wiphy */
@@ -1876,8 +1872,6 @@ struct wiphy {
 	u16 interface_modes;
 
 	u32 flags;
-
-	u32 ap_sme_capa;
 
 	enum cfg80211_signal_type signal_type;
 
